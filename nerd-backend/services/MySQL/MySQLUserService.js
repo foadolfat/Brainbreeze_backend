1713dd765ebcc06d669439a39e3ffcbb84f0e683
Foad Olfat <49184464+foadolfat@users.noreply.github.com>
2022-02-20 15:29:29 -0800

    NER-70, added authentication and general clean up for user route

--- nerd-backend/services/MySQL/MySQLUserService.js
+++ nerd-backend/services/MySQL/MySQLUserService.js
@@ -42,19 +42,56 @@ class MySQLUserService extends UserService {
         
     }
 
-    /**
+        /**
      * @param {import("../UserService").UserDTO} userDTO
      * @returns {Promise<Result<import("../UserService").User>>} 
      */
-    async getUser(userDTO){
+    async getUserByEmail(userDTO){
         /**
          * @type {Promise<import("../UserService").User>}
          */
         const getUserCMD = new Promise((resolve, reject) => {
             this.connection.query({
 			/* maybe change this later*/ 
-                sql:"SELECT *, CAST(user_password as CHAR) as user_password FROM user_table WHERE user_id=? OR user_email=?;",
-                values: [userDTO.user_id, userDTO.user_email]
+                sql:"SELECT *, CAST(user_password as CHAR) as user_password FROM user_table WHERE user_email=?;",
+                values: [userDTO.user_email]
+            }, (err, results) => {
+                
+                if(err){
+                    return reject(err);
+                }
+
+                if(!results || results.length === 0){
+                    var err = new Error("User does not exist!");
+                    err.errno = 1404;
+                    err.code = "NOT FOUND";
+                    return reject(err);
+                }
+                resolve(results[0]);
+            });
+        });
+        try{
+            const newUser = await getUserCMD;
+            return new Result(newUser, null);
+
+        } catch(e) {
+            return new Result(null, new IError(e.code, e.sqlMessage));
+        }
+    }
+
+    /**
+     * @param {import("../UserService").UserDTO} userDTO
+     * @returns {Promise<Result<import("../UserService").User>>} 
+     */
+    async getUserById(userDTO){
+        /**
+         * @type {Promise<import("../UserService").User>}
+         */
+        const getUserCMD = new Promise((resolve, reject) => {
+            this.connection.query({
+			/* maybe change this later*/ 
+                sql:"SELECT *, CAST(user_password as CHAR) as user_password FROM user_table WHERE user_id=?;",
+                values: [userDTO.user_id]
             }, (err, results) => {
                 
                 if(err){
@@ -86,14 +123,12 @@ class MySQLUserService extends UserService {
     async updateUser(userDTO) {
         const updateUserCMD = new Promise((resolve, reject) => {
             this.connection.query({
-                /* change password and type */
-                sql: "UPDATE user_table SET user_name=? WHERE user_id=?;",
-                values:[userDTO.user_name, userDTO.user_id]
+                sql: "UPDATE user_table SET user_name=?, user_type=? WHERE user_id=?;",
+                values:[userDTO.user_name, userDTO.user_type, userDTO.user_id]
             },
             (err, results) => {
                 
                 if(err) {
-                    
                     return reject(err);
                 }
                 resolve(results);
