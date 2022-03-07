c41951afcfa673cbac8cd0a33d995a5eacde57ea
Foad Olfat <49184464+foadolfat@users.noreply.github.com>
2022-03-06 17:22:38 -0800

    test2

--- nerd-backend/services/utility/AuthService.js
+++ nerd-backend/services/utility/AuthService.js
@@ -7,36 +7,7 @@ const saltRounds = 10;
 const secret= "super-duper-secret";
 
 async function authenticate(req, res, next){
-    /**
-    * @type {UserService}
-    */
-    const userService = ServiceLocator.getService(UserService.name);
-
-    try{
-        const { payload: user, error } = await userService.getUser(req.body);
-
-        if(error) {
-            res.status(400).json(error);
-        } else {
-            bcrypt.compare(req.body.password, user.password, function(err, result) {
-                if(err) {
-                    res.status(500).send("Internal Error");
-                    return;
-                }
-                req.auth = result;
-                if(result){
-                    req.token = jwt.sign({ id: user.id }, secret, {
-                        expiresIn: 86400 // 24 hours
-                    });
-                }
-                next();
-            });
-        }
-     }catch(e){
-         console.log("an error occured");
-         res.status(500).end();
-     }
-};
+    
 
 function encrypt(req, res, next){
     bcrypt.hash(req.body.user_password, saltRounds, function(err, hash) {
