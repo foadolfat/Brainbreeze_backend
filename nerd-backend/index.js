b84b897fb57a8c58825bfb7ddf592543c4d645ae
Foad Olfat <49184464+foadolfat@users.noreply.github.com>
2022-03-11 17:58:11 -0800

    adding swagger docs

--- nerd-backend/index.js
+++ nerd-backend/index.js
@@ -17,7 +17,23 @@ app.use(express.json());
 
 app.use(require(`./routes/userRoutes`));
 app.use(require(`./routes/classRoutes`));
+const swaggerJSDoc = require('swagger-jsdoc');
 
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API for JSONPlaceholder',
+    version: '1.0.0',
+  },
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 const databaseSetup = async () => {
     
